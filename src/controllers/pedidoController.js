e6a40159bb68a87120bc676cc511fa590d1ae597
felipeBarras <felipeu_santos@hotmail.com>
2025-01-11 22:23:21 -0300

    adicionando os controllers e services

--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -1,20 +1,28 @@
 const express = require('express');
+const { criarPedido, listarPedidos, buscarPedido, atualizarStatus } = require('../services/pedidoServices');
 const router = express.Router();
-const pedidoService = require('../services/pedidoServices');
 
 router.post('/', async (req, res) => {
-  const pedido = await pedidoService.criarPedido(req.body);
-
-  // Retorna o evento pedido para Produção
+  const { cliente, itens } = req.body;
+  const pedido = criarPedido({ cliente, itens });
   res.status(201).json(pedido);
 });
 
+router.get('/', async (req, res) => {
+  const pedidos = await listarPedidos();
+  res.status(200).json(pedidos);
+});
+
 router.get('/:id', async (req, res) => {
-  const pedido = await pedidoService.buscarPedido(req.params.id);
+  const pedido = await buscarPedido(req.params.id);
+  pedido ? res.json(pedido) : res.status(404).json({ message: 'Pedido não encontrado' });
+});
 
-  if (!pedido) return res.status(404).json({ message: 'Pedido não encontrado' });
+router.patch('/:id', async (req, res) => {
+  const { status } = req.body;
+  const pedido = await atualizarStatus(req.params.id, status);
 
-  res.json(pedido);
+  pedido ? res.json(pedido) : res.status(404).json({ message: 'Pedido não encontrado' });
 });
 
 module.exports = router;
