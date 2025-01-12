fed193d4176d5feea83b9b9ca2d1245c4bd18567
felipeBarras <felipeu_santos@hotmail.com>
2025-01-12 15:05:22 -0300

    refatorando para MongoDB, ao invés de SQL

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,20 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
+  user: process.env.POSTGRES_USER || 'pedidos_user',
   host: process.env.POSTGRES_HOST || 'localhost',
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_DB || 'pedidos_db',
+  password: process.env.POSTGRES_PASSWORD || 'pedido_pass',
   port: process.env.POSTGRES_PORT || 5432
+});
+
+pool.on('connect', () => {
+  console.log('Conexão com banco de dados estabelecida');
+});
+
+pool.on('error', (err) => {
+  console.error(`Erro inesperado no pool de conexão: ${err}`);
+  process.exit(-1);
 });
 
 module.exports = pool;
