8f3c97dff3b459ac57b749c6908824d0c99b8582
felipeBarras <felipeu_santos@hotmail.com>
2025-01-17 15:12:50 -0300

    finalizando as configurações dos testes

--- __tests__/PedidoController.test.js
+++ __tests__/PedidoController.test.js
@@ -2,6 +2,7 @@ const express = require('express');
 const request = require('supertest');
 const PedidoController = require('../src/infrastructure/http/PedidoController');
 const PedidoService = require('../src/application/PedidoService');
+const e = require('express');
 
 describe('Testes de PedidoController', () => {
   let app, mockPedidoService;
@@ -11,71 +12,73 @@ describe('Testes de PedidoController', () => {
     app.use(express.json());
 
     mockPedidoService = {
-      listarPedidos: jest.fn().mockResolvedValue([
-        { id: '1', cliente: 'Felipe', itens: [{ produto: 'Pizza', quantidade: 2 }], status: 'pendente' },
-        { id: '2', cliente: 'João', itens: [{ produto: 'Hamburguer', quantidade: 1 }], status: 'pago' }
-      ]),
-      buscarPedidoPorId: jest.fn().mockImplementation(async (id) => {
-        if (id === 1) return { id: '1', cliente: 'Felipe', itens: [{ produto: 'Pizza', quantidade: 2 }], status: 'pendente' };
-        return null;
-      }),
-      criarPedido: jest.fn().mockResolvedValue({
-        id: '3',
-        cliente: 'Karen',
-        itens: [{ produto: 'Esfiha', quantidade: 5 }],
-        status: 'pendente'
-      })
+      listarPedidos: jest.fn(),
+      buscarPedidoPorId: jest.fn(),
+      criarPedido: jest.fn()
     };
 
     app.use('/pedidos', PedidoController(mockPedidoService));
   });
 
   it('Deve listar todos os pedidos', async () => {
-    const response = await request(app).get('/pedidos');
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual([
+    const pedidos = [
       { id: '1', cliente: 'Felipe', itens: [{ produto: 'Pizza', quantidade: 2 }], status: 'pendente' },
-      { id: '2', cliente: 'João', itens: [{ produto: 'Hamburguer', quantidade: 1 }], status: 'pronto' }
-    ]);
+      { id: '2', cliente: 'João', itens: [{ produto: 'Hamburguer', quantidade: 1 }], status: 'pago' }
+    ];
+
+    mockPedidoService.mockResolvedValue(pedidos);
+
+    const response = await request(app).get('/pedidos');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(pedidos);
+    expect(mockPedidoService.listarPedidos).toHaveBeenCalled();
   });
 
   it('Deve retornar um 500 em caso de erro ao listar pedidos', async () => {
-    jest.spyOn(mockPedidoService, 'listarPedidos').mockImplementationOnce(async () => {
-      throw new Error('Erro ao listar pedidos');
-    });
+    mockPedidoService.listarPedidos.mockRejectedValue(new Error('Erro ao listar pedidos'));
 
     const response = await request(app).get('/pedidos');
+
     expect(response.status).toBe(500);
     expect(response.body).toEqual({ error: 'Erro ao listar pedidos' });
+    expect(mockPedidoService.listarPedidos).toHaveBeenCalled();
   });
 
   it('Deve buscar um pedido por seu ID', async () => {
-    const response = await request(app).get('/pedidos/1');
-
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual({
+    const pedido = {
       id: '1',
       cliente: 'Felipe',
       itens: [{ produto: 'Pizza', quantidade: 2 }],
       status: 'pendente'
-    });
+    };
+    mockPedidoService.buscarPedidoPorId.mockResolvedValue(pedido);
+
+    const response = await request(app).get('/pedidos/1');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(pedido);
+    expect(mockPedidoService.buscarPedidoPorId).toHaveBeenCalledWith('1');
   });
 
   it('Deve retornar um 404 caso o pedido não seja encontrado', async () => {
+    mockPedidoService.buscarPedidoPorId.mockResolvedValue(null);
+
     const response = await request(app).get('/pedidos/999');
 
     expect(response.status).toBe(404);
     expect(response.body).toEqual({ error: 'Pedido não encontrado' });
+    expect(mockPedidoService.buscarPedidoPorId).toHaveBeenCalledWith('999');
   });
 
   it('Deve retornar um 500 em caso de erro ao buscar um pedido', async () => {
-    jest.spyOn(mockPedidoService, 'buscarPedidoPorId').mockImplementationOnce(async () => {
-      throw new Error('Erro ao buscar pedido');
-    });
+    mockPedidoService.buscarPedidoPorId.mockRejectedValue(new Error('Erro ao buscar pedido'));
 
     const response = await request(app).get('/pedidos/1');
+
     expect(response.status).toBe(500);
     expect(response.body).toEqual({ error: 'Erro ao buscar pedido' });
+    expect(mockPedidoService.buscarPedidoPorId).toHaveBeenCalledWith('1');
   });
 
   it('Deve criar um pedido novo e retornar um status 201', () => {
@@ -85,6 +88,8 @@ describe('Testes de PedidoController', () => {
       status: 'pendente'
     };
 
+    mockPedidoService.criarPedido.mockResolvedValue({ id: '3', ...novoPedido });
+
     return (response = request(app)
       .post('/pedidos')
       .send(novoPedido)
@@ -92,6 +97,27 @@ describe('Testes de PedidoController', () => {
       .then((res) => {
         expect(res.status).toBe(201);
         expect(res.body).toEqual({ id: '3', ...novoPedido });
+        expect(mockPedidoService.criarPedido).toHaveBeenCalledWith(novoPedido);
+      }));
+  });
+
+  it('Deve retornar um 500 em caso de erro ao criar o pedido', async () => {
+    const novoPedido = {
+      cliente: 'Karen',
+      itens: [{ produto: 'Esfiha', quantidade: 5 }],
+      status: 'pendente'
+    };
+
+    mockPedidoService.criarPedido.mockRejectedValue(new Error('Erro ao criar pedido'));
+
+    return (response = request(app)
+      .post('/pedidos')
+      .send(novoPedido)
+      .expect(500)
+      .then((res) => {
+        expect(res.status).toBe(500);
+        expect(res.body).toEqual({ error: 'Erro ao criar pedido' });
+        expect(mockPedidoService.criarPedido).toHaveBeenCalledWith(novoPedido);
       }));
   });
 });
--- __tests__/PedidoService.test.js
+++ __tests__/PedidoService.test.js
@@ -1,62 +1,69 @@
 const PedidoService = require('../src/application/PedidoService');
+const MongoPedidoRepository = require('../src/infrastructure/database/MongoPedidoRepository');
 
 describe('Testes de PedidoService', () => {
   let pedidoService, mockPedidoGateway;
 
-  beforeAll(() => {
+  beforeEach(() => {
     mockPedidoGateway = {
-      listarPedidos: jest.fn().mockResolvedValue([
-        { id: '1', cliente: 'Felipe', itens: [{ produto: 'Pizza', quantidade: 2 }], status: 'pendente' },
-        { id: '2', cliente: 'João', itens: [{ produto: 'Hamburguer', quantidade: 1 }], status: 'pago' }
-      ]),
-      buscarPedidoPorId: jest.fn().mockImplementation(async (id) => {
-        if (id === 1) return { id: '1', cliente: 'Felipe', itens: [{ produto: 'Pizza', quantidade: 2 }], status: 'pendente' };
-        return null;
-      }),
-      criarPedido: jest.fn().mockResolvedValue({
-        id: '3',
-        cliente: 'Karen',
-        itens: [{ produto: 'Esfiha', quantidade: 5 }],
-        status: 'pendente'
-      })
+      criarPedido: jest.fn(),
+      salvarPedido: jest.fn(),
+      listarPedidos: jest.fn(),
+      buscarPedidoPorId: jest.fn()
     };
 
     pedidoService = new PedidoService(mockPedidoGateway);
   });
 
   it('Deve listar todos os pedidos', async () => {
-    const pedidos = await pedidoService.listarPedidos();
-
-    expect(pedidos).toEqual([
+    const pedidos = [
       { id: '1', cliente: 'Felipe', itens: [{ produto: 'Pizza', quantidade: 2 }], status: 'pendente' },
       { id: '2', cliente: 'João', itens: [{ produto: 'Hamburguer', quantidade: 1 }], status: 'pago' }
-    ]);
+    ];
+
+    mockPedidoGateway.listarPedidos.mockResolvedValue(pedidos);
+
+    const resultado = await pedidoService.listarPedidos();
+
+    expect(mockPedidoGateway.listarPedidos).toHaveBeenCalled();
+    expect(resultado).toEqual(pedidos);
   });
 
   it('Deve buscar um pedido por seu ID', async () => {
-    const pedido = await pedidoService.buscarPedidoPorId(1);
+    const pedido = {
+      id: '1',
+      cliente: 'Felipe',
+      itens: [{ produto: 'Pizza', quantidade: 2 }],
+      status: 'pendente'
+    };
+
+    mockPedidoGateway.buscarPedidoPorId.mockResolvedValue(pedido);
+
+    const resultado = await pedidoService.buscarPedidoPorId('1');
 
     expect(mockPedidoGateway.buscarPedidoPorId).toHaveBeenCalledWith(1);
-    expect(pedido).toEqual({ id: '1', cliente: 'Felipe', itens: [{ produto: 'Pizza', quantidade: 2 }], status: 'pendente' });
+    expect(resultado).toEqual(pedido);
   });
 
-  it('Deve retornar null se o pedido não for encontrado', async () => {
-    const pedido = await pedidoService.buscarPedidoPorId(999);
+  it('Deve lançar um erro se o pedido não for encontrado', async () => {
+    mockPedidoGateway.buscarPedidoPorId.mockResolvedValue('999');
 
-    expect(mockPedidoGateway.buscarPedidoPorId).toHaveBeenCalledWith(999);
-    expect(pedido).toBeNull();
+    await expect(pedidoService.buscarPedidoPorId('1')).rejects.toThrow('Pedido não encontrado');
+    expect(mockPedidoGateway.buscarPedidoPorId).toHaveBeenCalledWith('1');
   });
 
-  it('Deve criar um pedido novo', () => {
+  it('Deve criar um pedido novo', async () => {
     const novoPedido = {
       cliente: 'Karen',
       itens: [{ produto: 'Esfiha', quantidade: 5 }],
       status: 'pendente'
     };
 
-    const pedidoCriado = pedidoService.criarPedido(novoPedido);
+    mockPedidoGateway.salvarPedido.mockResolvedValue({ id: '3', ...novoPedido });
 
-    expect(mockPedidoGateway.criarPedido).toHaveBeenCalledWith(novoPedido);
+    const pedidoCriado = await pedidoService.criarPedido(novoPedido);
+
+    expect(mockPedidoGateway.salvarPedido).toHaveBeenCalledWith(expect.any(Object));
     expect(pedidoCriado).toEqual({ id: '3', ...novoPedido });
   });
 });
